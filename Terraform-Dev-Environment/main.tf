a8ecfb1d527a3fb3d2838349a2fce0e52d5de4eb
Samuel Nwanwobi <nwachiemi@gmail.com>
2025-03-29 23:54:00 +0100

    added internet gateway and route table

--- Terraform-Dev-Environment/main.tf
+++ Terraform-Dev-Environment/main.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "dev_vpc" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
@@ -9,12 +9,34 @@ resource "aws_vpc" "dev_vpc" {
 }
 
 resource "aws_subnet" "dev_public_subnet" {
+  vpc_id                  = aws_vpc.dev_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-west-2a"
+
+  tags = {
+    Name = "dev-public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "dev_internet_gateway" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  tags = {
+    Name = "dev-internet-gateway"
+  }
+}
+
+resource "aws_route_table" "dev_public_route_table" {
     vpc_id = aws_vpc.dev_vpc.id
-    cidr_block = "10.0.1.0/24"
-    map_public_ip_on_launch = true
-    availability_zone = "us-west-2a"
 
     tags = {
-        Name = "dev-public-subnet"
+        Name = "dev-public-route-table"
     }
+}
+
+resource "aws_route" "dev_public_route" {
+    route_table_id = aws_route_table.dev_public_route_table.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dev_internet_gateway.id
 }
